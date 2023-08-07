e1b63f54886d8b5642412056a54fe2cf02368354
Willy <willy@coinerella.com>
2023-08-07 17:23:59 +0200

    restructure and get ARS price from yadio

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 async function gatherResponse(response) {
-  const { headers } = response
-  const contentType = headers.get('content-type') || ''
+  const { headers } = response;
+  const contentType = headers.get('content-type') || '';
   if (contentType.includes('application/json')) {
-    return await response.json()
+    return await response.json();
   } else {
-    return response.text()
+    return response.text();
   }
 }
 
@@ -15,64 +15,167 @@ async function getFromApi(url, authHeader = "") {
       'Authorization': "Basic " + authHeader,
     },
   }
-  const response = await fetch(url, init)
-  const results = await gatherResponse(response)
-  return results
+  const response = await fetch(url, init);
+  const results = await gatherResponse(response);
+  return results;
 }
 
 addEventListener('fetch', (event) => {
   event.waitUntil(handleSchedule())
-  return event.respondWith(new Response())
+  return event.respondWith(new Response());
 })
 
 addEventListener('scheduled', (event) => {
-  event.waitUntil(handleSchedule())
+  event.waitUntil(handleSchedule());
 })
 
 async function handleSchedule() {
   //get coinpaprika ppc/usd price
   const paprikaResponse = await getFromApi(
     'https://paprika.ppc.lol/v1/tickers/ppc-peercoin', PAPRIKA_TOKEN
-  )
+  );
+
   //get currency fiat/usd exchange rates
   const openExchangeResponse = await getFromApi(
     `https://openexchangerates.org/api/latest.json?app_id=${CURRCONV_KEY}`,
-  )
+  );
 
-  const ppcUsdPrice = paprikaResponse['quotes']['USD']['price']
-  const rates = openExchangeResponse['rates']
+  const rates = openExchangeResponse['rates'];
+  const enabledCurrencies = [
+    "AUD",
+    "BDT",
+    "BRL",
+    "CNY",
+    "DKK",
+    "EUR",
+    "GBP",
+    "IDR",
+    "INR",
+    "IRR",
+    "JPY",
+    "KES",
+    "KRW",
+    "NOK",
+    "PHP",
+    "PKR",
+    "PLN",
+    "RON",
+    "RUB",
+    "SEK",
+    "THB",
+    "TRY",
+    "TZS",
+    "UAH",
+    "UGX",
+    "VND",
+    "AED",
+    "ALL",
+    "AMD",
+    "AOA",
+    "AZN",
+    "BAM",
+    "BDT",
+    "BGN",
+    "BHD",
+    "BIF",
+    "BND",
+    "BOB",
+    "BSD",
+    "BTN",
+    "BWP",
+    "BYN",
+    "CAD",
+    "CDF",
+    "CHF",
+    "CLP",
+    "COP",
+    "CRC",
+    "CVE",
+    "CZK",
+    "DOP",
+    "EGP",
+    "GEL",
+    "GMD",
+    "GNF",
+    "GTQ",
+    "GYD",
+    "HKD",
+    "HNL",
+    "HRK",
+    "HUF",
+    "ILS",
+    "IQD",
+    "ISK",
+    "JMD",
+    "JOD",
+    "KGS",
+    "KMF",
+    "KWD",
+    "KZT",
+    "LAK",
+    "LBP",
+    "LKR",
+    "LRD",
+    "LSL",
+    "LYD",
+    "MAD",
+    "MDL",
+    "MGA",
+    "MKD",
+    "MNT",
+    "MOP",
+    "MUR",
+    "MVR",
+    "MWK",
+    "MYR",
+    "MXN",
+    "MZN",
+    "NAD",
+    "NGN",
+    "NZD",
+    "OMR",
+    "PEN",
+    "PYG",
+    "QAR",
+    "RSD",
+    "RWF",
+    "SAR",
+    "SCR",
+    "SDG",
+    "SGD",
+    "SLL",
+    "STD",
+    "SZL",
+    "TJS",
+    "TND",
+    "TTD",
+    "TWD",
+    "UYU",
+    "XAF",
+    "XOF",
+    "XPF",
+    "ZAR",
+    "ZMK"
+  ];
 
-  const prices = {
-    ARS: rates['ARS'],
-    AUD: rates['AUD'],
-    BDT: rates['BDT'],
-    BRL: rates['BRL'],
-    CNY: rates['CNY'],
-    DKK: rates['DKK'],
-    EUR: rates['EUR'],
-    GBP: rates['GBP'],
-    IDR: rates['IDR'],
-    INR: rates['INR'],
-    IRR: rates['IRR'],
-    JPY: rates['JPY'],
-    KES: rates['KES'],
-    KRW: rates['KRW'],
-    NOK: rates['NOK'],
-    PHP: rates['PHP'],
-    PKR: rates['PKR'],
-    PLN: rates['PLN'],
-    PPC: parseFloat(ppcUsdPrice.toFixed(6)),
-    RON: rates['RON'],
-    RUB: rates['RUB'],
-    SEK: rates['SEK'],
-    THB: rates['THB'],
-    TRY: rates['TRY'],
-    TZS: rates['TZS'],
-    UAH: rates['UAH'],
-    UGX: rates['UGX'],
-    VND: rates['VND']
+  const prices = {};
+  // loop over enabledCurrencies for prices that are availble on openexchangerates
+  for (const currency of enabledCurrencies) {
+    prices[currency] = rates[currency];
   }
 
+  // add PPC 
+  const ppcUsdPrice = paprikaResponse['quotes']['USD']['price'];
+  prices['PPC'] = parseFloat(ppcUsdPrice.toFixed(6));
+
+  // add ARS TODO
+  const yadioResponse = await getFromApi('https://api.yadio.io/exrates/USD');
+  const arsUsdPrice = yadioResponse['USD']['ARS'];
+  prices['ARS'] = arsUsdPrice;
+
+  // sort prices by key
+  const sortedPrices = Object.fromEntries(Object.entries(prices).sort());
+
   //write to KV
-  await peercoin_kv.put('prices', JSON.stringify(prices))
+  await peercoin_kv.put('prices', JSON.stringify(sortedPrices));
 }
